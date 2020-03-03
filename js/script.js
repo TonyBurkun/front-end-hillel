c0a603e7ca2352d166c37dca3c2e709374aad92f
Anton Burkun <burkun.anton@gmail.com>
2020-03-03 18:51:27 +0200

    start home work for the lesson 9

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// var obj = Object.create(Object.prototype, {
+//     a: {
+//         enumerable: false,
+//         value: 2
+//     },
+//     b: {
+//         value: 3,
+//         enumerable: true
+//     }
+// });
+//
+// var obj2 = {
+//     a: 2
+// };
+//
+// for (var key in obj) {
+//     console.log(key);
+// }
+
+
+//Object.prototype
+
+//{}.__proto__
