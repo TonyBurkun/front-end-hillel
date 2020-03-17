9740d5ed5faa6c88e4811caf0c35138d9014b7cd
Anton Burkun <burkun.anton@gmail.com>
2020-03-17 23:30:02 +0200

    lesson_13 init

--- js/homework.js
+++ js/homework.js
@@ -1,86 +1,12 @@
-function MilitaryResource(type, health, distance, maxHealth = 100, maxDistance = 20) {
-    this.type = type;
-    this.health = health;
-    this.maxHealth = maxHealth;
-    this.distance = distance;
-    this.maxDistance = maxDistance;
+function sum() {
+
+    let result = 0;
+    for (let i=0; i< arguments.length; i++) {
+        result += arguments[i];
+    }
+    return result;
 }
 
+var result = sum(1,2,3,4,5,6); //21
 
-MilitaryResource.prototype.isReadyToMove = function (){
-    return this.distance > 0;
-};
-
-MilitaryResource.prototype.isReadyToFight = function () {
-    return this.health > 10;
-};
-
-MilitaryResource.prototype.restore = function () {
-    this.health = this.maxHealth;
-    this.distance = this.maxDistance;
-};
-
-MilitaryResource.prototype.clone = function () {
-    return new MilitaryResource(this.type, this.health, this.distance);
-};
-
-
-
-
-function Squad(defaultResources) {
-    this.squad = [];
-    if (defaultResources) this.combineResources(defaultResources);
-}
-
-
-Squad.prototype.isReadyToMove = function() {
-    return this.squad.every(item => {
-        return item.isReadyToMove();
-    })
-};
-
-Squad.prototype.isReadyToFight = function () {
-    return this.squad.some(item => {
-        return item.isReadyToFight();
-    })
-};
-
-Squad.prototype.restore = function () {
-    this.squad.forEach(item => {
-        item.restore();
-    });
-
-    return this.squad;  // or should I return true as result of restore action
-};
-
-Squad.prototype.getReadyToMoveResources = function () {
-   return this.squad.filter(item => {
-        return item.isReadyToMove()
-    });
-};
-
-Squad.prototype.combineResources = function (resources) {
-    this.squad = [...this.squad, ...resources]
-};
-
-Squad.prototype.cloneResources = function (resources) {
-    return resources.map(item => {
-        return item.clone();
-    })
-};
-
-
-let solder = new MilitaryResource('solder', 11, 15);
-let solder2 = new MilitaryResource('solder2', 11, 1);
-
-
-let warriors = new Squad([solder, solder2]);
-
-
-
-
-
-
-
-
-
+console.log(result);
