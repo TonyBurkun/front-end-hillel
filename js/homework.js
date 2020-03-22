9c2b7d85fa02fe194f4ebcd0804c7d5f7c1888db
Anton Burkun <burkun.anton@gmail.com>
2020-03-22 13:52:36 +0200

    lesson_13 homework

--- js/homework.js
+++ js/homework.js
@@ -1,12 +1,48 @@
-function sum() {
-
-    let result = 0;
-    for (let i=0; i< arguments.length; i++) {
-        result += arguments[i];
+var calculations = {
+    salarySum: function() {
+        return this.reduce(function(acc, next) {
+            return acc + next;
+        }, 0);
+    },
+    logName: function() {
+        this.forEach(function(user) {
+            console.log(user.name);
+        });
+    },
+    findUsersByIds: function(ids) {
+        return this.filter(function(user) {
+            return ids.find(function(id) {
+                return user.id === id;
+            })
+        });
     }
-    return result;
-}
+};
 
-var result = sum(1,2,3,4,5,6); //21
+var users = [
+    {
+        id: 1,
+        name: "User 2",
+        salary: 200
+    },
+    {
+        id: 2,
+        name: "User 3",
+        salary: 200
+    },
+    {
+        id: 3,
+        name: "User 4",
+        salary: 200
+    }
+];
 
-console.log(result);
+
+let usersSalaryArr = users.map(item => {
+    return item.salary;
+});
+console.log(calculations.salarySum.call(usersSalaryArr));
+
+calculations.logName.call(users);
+
+
+console.log(calculations.findUsersByIds.call(users, [2, 3]));
