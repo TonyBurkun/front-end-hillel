4c37c7fbbe8f2f66e95e1a6119e7d1e1e5e41d04
Anton Burkun <burkun.anton@gmail.com>
2020-03-24 15:01:19 +0200

    lesson_14 homework

--- js/homework.js
+++ js/homework.js
@@ -1,12 +1,68 @@
-function sum() {
+// Реализовать наследования следующей цепочки:
+//     Животное -> Млекопитающее -> Енот
+// Между этими прослойками можно создать еще несколько своих =)
+// Прототип животного должен иметь 2 метода передвижения (пр. прыжок, бег) и свойство пол
+// Прототип млекопитающего должен иметь метод, который, в зависимости от пола дает молоко.
+// Прототип енота должен уметь воровать =)
 
-    let result = 0;
-    for (let i=0; i< arguments.length; i++) {
-        result += arguments[i];
-    }
-    return result;
+
+const GENDER = {
+    male: 'male',
+    female: 'female'
+
+};
+
+function Animal(color, gender) {
+    this.gender = gender || GENDER.male;
+    this.color = color || null;
 }
 
-var result = sum(1,2,3,4,5,6); //21
+Animal.prototype.jumping = function () {
+    console.log('Jumping')
+};
 
-console.log(result);
+Animal.prototype.running = function () {
+    console.log('Running');
+};
+
+
+
+let crocodile = new Animal('green', GENDER.female);
+
+console.log(crocodile);
+
+function Mammal(color, gender) {
+    Animal.call(this, color, gender);
+}
+
+Mammal.prototype = Object.create(Animal.prototype);
+Mammal.prototype.constructor = Animal;
+
+
+Mammal.prototype.givesMilk = function () {
+    return this.gender === GENDER.female;
+};
+
+let cow = new Mammal('black', GENDER.female);
+let bull = new Mammal();
+
+console.log(cow);
+console.log(bull);
+
+
+function Raccoon(color, gender){
+    Mammal.call(this, color, gender)
+}
+
+Raccoon.prototype = Object.create(Mammal.prototype);
+Raccoon.prototype.constructor = Mammal;
+
+Raccoon.prototype.canSteal = function() {
+    console.log('Available to steal');
+};
+
+let peterRaccoon = new Raccoon();
+
+
+
+
