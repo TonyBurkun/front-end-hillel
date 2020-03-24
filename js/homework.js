0de4bbe3e56edf0cca0c1e49e10cf0aebc2646ef
Anton Burkun <burkun.anton@gmail.com>
2020-03-24 15:41:39 +0200

    lesson_11 homework

--- js/homework.js
+++ js/homework.js
@@ -12,55 +12,9 @@ const GENDER = {
 
 };
 
-function Animal(color, gender) {
-    this.gender = gender || GENDER.male;
-    this.color = color || null;
-}
-
-Animal.prototype.jumping = function () {
-    console.log('Jumping')
-};
-
-Animal.prototype.running = function () {
-    console.log('Running');
-};
-
-
-
 let crocodile = new Animal('green', GENDER.female);
-
-console.log(crocodile);
-
-function Mammal(color, gender) {
-    Animal.call(this, color, gender);
-}
-
-Mammal.prototype = Object.create(Animal.prototype);
-Mammal.prototype.constructor = Animal;
-
-
-Mammal.prototype.givesMilk = function () {
-    return this.gender === GENDER.female;
-};
-
 let cow = new Mammal('black', GENDER.female);
 let bull = new Mammal();
-
-console.log(cow);
-console.log(bull);
-
-
-function Raccoon(color, gender){
-    Mammal.call(this, color, gender)
-}
-
-Raccoon.prototype = Object.create(Mammal.prototype);
-Raccoon.prototype.constructor = Mammal;
-
-Raccoon.prototype.canSteal = function() {
-    console.log('Available to steal');
-};
-
 let peterRaccoon = new Raccoon();
 
 
