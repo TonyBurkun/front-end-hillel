9151262e9bd20a99eedb57ec43fdf4ec66467a77
Anton Burkun <burkun.anton@gmail.com>
2020-05-06 15:59:44 +0300

    lesson 15 homework

--- js/homework.js
+++ js/homework.js
@@ -24,14 +24,10 @@ MilitaryResource.prototype.clone = function () {
     return new MilitaryResource(this.type, this.health, this.distance);
 };
 
-
-
-
 function Squad(defaultResources) {
     this.squad = [];
     if (defaultResources) this.combineResources(defaultResources);
 }
-
 
 Squad.prototype.isReadyToMove = function() {
     return this.squad.every(item => {
@@ -69,12 +65,42 @@ Squad.prototype.cloneResources = function (resources) {
     })
 };
 
-
 let solder = new MilitaryResource('solder', 11, 15);
 let solder2 = new MilitaryResource('solder2', 11, 1);
 
 
 let warriors = new Squad([solder, solder2]);
+
+
+let unit = new MilitaryResource('Warrior', 300, 150);
+let unit2 = new MilitaryResource('Peasant', 300, 150);
+
+let wrapper = document.getElementById('wrapper');
+
+function createUnitCard(unit, parent){
+    let type = createElement('h3', {classes: ['title']});
+    type.innerHTML = unit.type;
+
+    let healthValue = createElement('span', {classes: ['bar-value']});
+    healthValue.innerHTML = unit.maxHealth;
+
+    let staminaValue = createElement('span', {classes: ['bar-value']});
+    staminaValue.innerHTML = unit.maxDistance;
+
+    let health = createElement('div', {classes: ['bar', 'health']}, [healthValue]);
+    let stamina = createElement('div', {classes: ['bar', 'stamina']}, [staminaValue]);
+    let avatar = createElement('img', {classes: ['avatar']});
+
+    let card = createElement('div', {classes: ['card']}, [type, health, stamina, avatar]);
+
+    parent.append(card);
+}
+
+createUnitCard(unit, wrapper);
+createUnitCard(unit, wrapper);
+createUnitCard(unit, wrapper);
+createUnitCard(unit2, wrapper);
+createUnitCard(unit2, wrapper);
 
 
 
